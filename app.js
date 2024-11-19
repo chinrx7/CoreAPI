d5da5ddd4da47c74767be400d2a81242857413d2
FV_SEATRIAL\SATAdmin <chinaphatr@gmail.com>
2024-11-19 15:51:26 +0700

    wasd

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const os = require('os');
 const path = require('path');
 const routes = require('./routes/route');
 const logger = require('./middleware/log');
-const compression = require('compression')
+const compression = require('compression');
+const https = require('https');
+const fs = require('fs');
 
 const { config } = require('./middleware/config');
 
@@ -20,8 +22,18 @@ app.use(cors({ origin: '*', credentials: true }));
 app.use(compression());
 
 const Port = config.PORT || 4123;
+const Ports = config.PORTS || 443;
+
+const key = fs.readFileSync('./cer/selfsigned.key');
+const cert = fs.readFileSync('./cer/selfsigned.crt');
+const options = {
+    key: key,
+    cert: cert
+}
 
 app.use(routes);
+
+const servers = https.createServer(options, app);
 
 let nThread=0;
 
@@ -45,5 +57,9 @@ if (cluster.isMaster) {
 } else {
     app.listen(Port, () => {
         logger.loginfo('CoreAPI', `Started and listen at port: ${Port} and Process ID: ${process.pid}`, 'Info');
+    });
+
+    servers.listen(Ports, () => {
+        logger.loginfo('CoreAPI', `Hppts Listen at port: ${Ports}`);
     })
 } 
